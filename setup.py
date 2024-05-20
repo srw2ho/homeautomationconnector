f7b2a7edb26d8445e3f7017f52ddd84e90ffb9d2
balciselman <selman.balci@gmail.com>
2024-05-20 14:26:14 +0200

    setup changes:lgpio added

--- setup.py
+++ setup.py
@@ -3,12 +3,11 @@ import setuptools
 
 NAME = "homeautomationconnector"
 
-DEPENDENCIES_ARTIFACTORY = [
-    "gpiozero",
-    "RPi.GPIO",
-    "suntime",
-    "numpy"
-]
+DEPENDENCIES_ARTIFACTORY = ["gpiozero", 
+                            "RPi.GPIO", # gpiozero - Pin factory raspy 4
+                            "lgpio",    # gpiozero - Pin factory raspy 5: neueste gpiozero-Version sucht zuerst nach lgpio, danach ... RPi.GPIO
+                            "suntime", 
+                            "numpy"]
 
 
 DEPENDENCIES_GITHUB = {
@@ -21,20 +20,24 @@ DEPENDENCIES_GITHUB = {
 
 
 def generate_pip_links_from_url(url, version):
-    """ Generate pip compatible links from Socialcoding clone URLs
+    """Generate pip compatible links from Socialcoding clone URLs
 
     Arguments:
         url {str} -- Clone URL from Socialcoding
     """
-    package = url.split('/')[-1].split('.')[0]
+    package = url.split("/")[-1].split(".")[0]
     url = url.replace("https://", f"{package} @ git+https://")
     if version:
         url = url + f"@{version}"
 
     return url
 
+
 # create pip compatible links
-DEPENDENCIES_GITHUB = [generate_pip_links_from_url(url, version) for url, version in DEPENDENCIES_GITHUB.items()]
+DEPENDENCIES_GITHUB = [
+    generate_pip_links_from_url(url, version)
+    for url, version in DEPENDENCIES_GITHUB.items()
+]
 DEPENDENCIES = DEPENDENCIES_ARTIFACTORY + DEPENDENCIES_GITHUB
 
 with open("README.md", "r") as fh:
@@ -46,7 +49,6 @@ setuptools.setup(
     setuptools_git_versioning={
         "enabled": True,
     },
-
     author="srw2ho",
     author_email="",
     description="",
@@ -57,11 +59,10 @@ setuptools.setup(
     package_data={},
     install_requires=DEPENDENCIES,
     setup_requires=[
-        'setuptools-git-versioning<2',
+        "setuptools-git-versioning<2",
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: Other/Proprietary License"
-        "Operating System :: OS Independent",
+        "License :: Other/Proprietary License" "Operating System :: OS Independent",
     ],
 )
